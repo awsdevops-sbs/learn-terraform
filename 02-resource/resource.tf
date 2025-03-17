3baadf05e914735df30ab243ec852dab81126fd9
akram <akram.khan200@gmail.com>
2025-03-17 21:47:50 +0530

    terrform output13

--- 02-resource/resource.tf
+++ 02-resource/resource.tf
@@ -10,7 +10,7 @@ resource "aws_spot_instance_request" "Frontend_terrform" {
   ami                  = "ami-09c813fb71547fc4f"
   instance_type        = "t3.micro"
   spot_price           = "0.02"
-  spot_instance_type   = "persistent"   # This replaces the 'persistent' argument
+  spot_type = "persistent"  # This replaces the 'persistent' argument
   instance_interruption_behavior = "stop"  # Stops instead of terminating
 
   vpc_security_group_ids = ["sg-0262c4232ab2a8184"]
