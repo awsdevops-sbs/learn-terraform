d82d9d2bb7cf6c0730c3caeadc7649338e577762
akram <akram.khan200@gmail.com>
2025-03-20 00:49:16 +0530

    terrform output21

--- 04-Data/main.tf
+++ 04-Data/main.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.81.0"
-    }
-  }
-}
-data "aws_security_groups" "selected" {
+data "aws_security_group" "selected" {
 
   name = "allow-all"
 
