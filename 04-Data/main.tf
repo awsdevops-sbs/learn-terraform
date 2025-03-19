8e35a63dbc4f320c6e05db43fae0f7fc6b11d80d
akram <akram.khan200@gmail.com>
2025-03-20 00:52:39 +0530

    terrform output21

--- 04-Data/main.tf
+++ 04-Data/main.tf
@@ -1,9 +1,9 @@
-data "aws_security_group" "selected" {
+data "aws_security_group" "local" {
 
   name = "Allow-all-from-public"
 
 }
 
 output "sec" {
-  value = data.aws_security_group.selected
+  value = data.aws_security_group.local.id
 }
