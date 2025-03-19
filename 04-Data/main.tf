a69bbae6a8cc0655e87e40927e3a4e563781d0d1
akram <akram.khan200@gmail.com>
2025-03-20 01:00:43 +0530

    terrform output21

--- 04-Data/main.tf
+++ 04-Data/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 data "aws_security_group" "local" {
 
   name = "Allow-all-from-public"
@@ -6,4 +14,17 @@ data "aws_security_group" "local" {
 
 output "sec" {
   value = data.aws_security_group.local.id
+}
+
+
+data "aws_instance" "test" {
+
+  name = "frontend"
+}
+
+output "check" {
+  value = [ data.aws_instance.test.name ,
+            data.aws_instance.test.private_ip,
+            data.aws_instance.test.private_dns
+          ]
 }
