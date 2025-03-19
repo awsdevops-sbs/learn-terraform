2007ce06f6bdd983103189ddaf22a7d15e375dfe
akram <akram.khan200@gmail.com>
2025-03-19 23:39:22 +0530

    terrform output15

---
+++ expenses-v1/main.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "instance_types" {
+ for_each = var.instance_types
+ ami = var.ami
+ instance_type = each.value["instance_type"]
+ vpc_security_group_ids = var.sec
+
+  tags = {
+
+    Name = each.key
+  }
+}
+
+variable "ami" {
+
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "sec" {
+
+  default = ["sg-0262c4232ab2a8184"]
+}
+
+variable "instance_types" {
+
+  default = {
+
+    tforntend = {
+
+      instance_type = "t3.micro"
+    }
+
+    tbackend = {
+
+      instance_type = "t3.micro"
+    }
+
+    tmysql = {
+
+      instance_type = "t3.small"
+    }
+  }
+}
+
